d41f2ad607266e6d6170db29ea1df157649a860b
Kevin Paucar <kpaucar05@gmail.com>
2023-01-05 09:21:25 -0600

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
+const postBank = require("./postBank");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const app = express();
+app.get(express.static('public/style.css'));
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+  
+  const posts = postBank.list();
+  
+  const html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+    </head>
+    <body>
+      <ul>
+      ${
+        posts.map( post => {
+          return `<li>${ post.name}</li>
+          <p>${post.title}</p>`;
+        }).join('')
+     }
+      </ul>
+    </body>
+  </html>`;
+
+  res.send(html);
+});
 
 const PORT = 1337;
 
